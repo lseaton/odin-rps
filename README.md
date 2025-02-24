132472830d590b3136a04c5a19b1bd44fa477ca1
lseaton <73501741+lseaton@users.noreply.github.com>
2025-02-23 20:14:06 -0800

    Implement logic for scales moving via buttons

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 <p>--Pixabay credits--</p>
 <p>Audio from <a href="https://pixabay.com/users/freesound_community-46691455/">freesound_community</a>.</p>
 <p>Wizard, rock, manuscript, scissors, and skull dungeon images from <a href="https://pixabay.com/users/openclipart-vectors-30363/">OpenClipart-Vectors</a>.</p>
-<p>Knight image from <a href="https://pixabay.com/users/silviap_design-1583911/">SilviaP_Design</a></p>
+<p>Knight image from <a href="https://pixabay.com/users/silviap_design-1583911/">SilviaP_Design</a>.</p>
--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -39,3 +39,74 @@ if (humanScore != computerScore) {
 } else {
 	console.log("The game is a tie!");
 }*/
+
+////////////////////////////
+////// Scales section //////
+////////////////////////////
+//Array for the matrices representing different rotations, ascending from -15 to 15 by fives
+const matRotation = [
+	"matrix(0.965926, -0.258819, 0.258819, 0.965926, 0, 0)",
+	"matrix(0.984808, -0.173648, 0.173648, 0.984808, 0, 0)",
+	"matrix(0.996195, -0.0871557, 0.0871557, 0.996195, 0, 0)",
+	"matrix(1, 0, 0, 1, 0, 0)",
+	"matrix(0.996195, 0.0871557, -0.0871557, 0.996195, 0, 0)",
+	"matrix(0.984808, 0.173648, -0.173648, 0.984808, 0, 0)",
+	"matrix(0.965926, 0.258819, -0.258819, 0.965926, 0, 0)",
+];
+const degRotation = [-15, -10, -5, 0, 5, 10, 15];
+const bar = document.getElementById("bar");
+const left = document.getElementById("left");
+const right = document.getElementById("right");
+let leftDivs = Array.prototype.slice.call(left.children); //all div children of the left side, as an array
+let rightDivs = Array.prototype.slice.call(right.children); //all div children of the right side, as an array
+
+function getNewBarRotation(step) {
+	let matrix = window.getComputedStyle(bar).transform.toString(); //retrieves the current rotation matrix in a string form
+	if (matrix == "none") {
+		//this sometimes happens when the bar is level
+		newRotation = degRotation[3 + step];
+	} else {
+		let rotationIndex = matRotation.indexOf(matrix);
+		var newRotation;
+		if (rotationIndex + step >= 0 || rotationIndex + step <= 6) {
+			newRotation = degRotation[rotationIndex + step];
+		} else {
+			newRotation = degRotation[rotationIndex];
+		}
+	}
+	return newRotation;
+}
+
+function getNewTransforms(rotation) {
+	let lyt = rotation * -2; //left y transform
+	let ryt = rotation * 2; //right y transform
+	if (rotation != 0) {
+		//minor adjustments to make it look better
+		if (rotation == Math.abs(rotation)) {
+			lyt += 5;
+		} else {
+			ryt += 5;
+		}
+	}
+	return [lyt, ryt];
+}
+
+function changeWeight(val) {
+	let br = getNewBarRotation(val); //bar rotation
+	let lyt = getNewTransforms(br)[0]; //left y transform
+	let ryt = getNewTransforms(br)[1]; //right y transform
+	//rotate bar
+	bar.style.transform = "rotate(" + br + "deg)";
+	//transform left elements down and right elements up
+	leftDivs.forEach((d) => (d.style.transform = "translateY(" + lyt + "px)"));
+	rightDivs.forEach(
+		(d) => (d.style.transform = "translate(200px, " + ryt + "px)")
+	);
+}
+
+function leftButton() {
+	changeWeight(-1);
+}
+function rightButton() {
+	changeWeight(1);
+}
