1f67cc9c339cdccd7cd85f9c0c7eaddb817816f8
lseaton <73501741+lseaton@users.noreply.github.com>
2025-02-25 03:35:08 -0800

    Add logic for game ending

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -59,7 +59,7 @@ function focusUp(c) {
 }
 function unfocus(c) {
 	document.querySelector(c + " .shimmer").style.visibility = "hidden";
-	followMouse(c);
+	unfollowMouse(c);
 }
 
 function playRound(humanChoice) {
@@ -72,19 +72,32 @@ function playRound(humanChoice) {
 
 	if (humanIndex != computerIndex) {
 		if (computerIndex + 2 == humanIndex || computerIndex - 1 == humanIndex) {
+			//Computer wins this round
 			changeWeight(-1);
-			computerScore++;
-			message.textContent +=
-				"and " + computerChoice + " beats " + humanChoice + ". I win.";
-			playSound(computerChoice);
+			if (!gameOver()) {
+				computerScore++;
+				message.textContent +=
+					"and " + computerChoice + " beats " + humanChoice + ". I win.";
+				playSound(computerChoice);
+			} else {
+				//Computer wins game
+				computerWins();
+			}
 		} else {
+			//Human wins this round
 			changeWeight(1);
-			humanScore++;
-			message.textContent +=
-				"but " + humanChoice + " beats " + computerChoice + ". You win.";
-			playSound(humanChoice);
+			if (!gameOver()) {
+				humanScore++;
+				message.textContent +=
+					"but " + humanChoice + " beats " + computerChoice + ". You win.";
+				playSound(humanChoice);
+			} else {
+				//Human wins game
+				humanWins();
+			}
 		}
 	} else {
+		//Tie
 		message.textContent += "so it's a tie.";
 		playSound(computerChoice);
 	}
@@ -108,13 +121,54 @@ function playSound(sound) {
 	}
 }
 
+//Check if game is over, return string indicating the winner if so and false if not
+function gameOver() {
+	let matrix = window.getComputedStyle(bar).transform.toString();
+	if (matrix == matRotation[0]) {
+		return "computerWin";
+	} else if (matrix == matRotation[6]) {
+		return "humanWin";
+	} else {
+		return false;
+	}
+}
+
+function humanWins() {
+	message.textContent =
+		"Your intuition has served you well! You are now a knight of the realm.";
+	let weapons = document.querySelectorAll(".options div");
+	let weaponClasses = [".rock", ".paper", ".scissors"];
+	for (let i = 0; i < weaponClasses.length; i++) {
+		unfocus(weaponClasses[i]);
+	}
+	for (let i = 0; i < weapons.length; i++) {
+		weapons[i].onclick = null;
+		weapons[i].onmouseover = null;
+		weapons[i].style.opacity = "0.5";
+	}
+}
+function computerWins() {
+	message.textContent =
+		"Your intuition has failed you. You shall not become a knight today.";
+	let weapons = document.querySelectorAll(".options div");
+	let weaponClasses = [".rock", ".paper", ".scissors"];
+	for (let i = 0; i < weaponClasses.length; i++) {
+		unfocus(weaponClasses[i]);
+	}
+	for (let i = 0; i < weapons.length; i++) {
+		weapons[i].onclick = null;
+		weapons[i].onmouseover = null;
+		weapons[i].style.opacity = "0.5";
+	}
+}
+//Make the scales appear to be weighed more in one direction
 function changeWeight(val) {
 	let br = getNewBarRotation(val); //bar rotation
 	let lyt = getNewTransforms(br)[0]; //left y transform
 	let ryt = getNewTransforms(br)[1]; //right y transform
 	//rotate bar
 	bar.style.transform = "rotate(" + br + "deg)";
-	//transform left elements down and right elements up
+	//transform left elements and right elements, one down and one up
 	leftDivs.forEach((d) => (d.style.transform = "translateY(" + lyt + "px)"));
 	rightDivs.forEach(
 		(d) => (d.style.transform = "translate(200px, " + ryt + "px)")
@@ -152,7 +206,7 @@ function getNewTransforms(rotation) {
 	return [lyt, ryt];
 }
 
-/////Code in this section below modified from an article by Armando Canals for the 3D effect.
+/////Code in this section below modified from an article by Armando Canals for a 3D effect.
 // https://www.armandocanals.com/posts/CSS-transform-rotating-a-3D-object-perspective-based-on-mouse-position.html.
 function followMouse(weaponClass) {
 	let constrain = 20;
@@ -189,3 +243,8 @@ function followMouse(weaponClass) {
 	};
 }
 //////End section for Armando Canals
+
+function unfollowMouse(weaponClass) {
+	let mouseOverContainer = document.querySelector(weaponClass);
+	mouseOverContainer.onmousemove = null;
+}
